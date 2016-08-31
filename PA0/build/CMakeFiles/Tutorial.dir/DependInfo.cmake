84ae049f2b9c412d0091c9480612fc6fabf1baf7
sstukelja <sstukelja@nevada.unr.edu>
2016-08-31 10:15:48 -0700

    Add files via upload

---
+++ PA0/build/CMakeFiles/Tutorial.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/camera.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/engine.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/graphics.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/main.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/object.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/shader.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "/home/sstukelja/CS480/computer-graphics/PA0/src/window.cpp" "/home/sstukelja/CS480/computer-graphics/PA0/build/CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "UNIX"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# The include file search paths:
+SET(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  "/usr/include/SDL2"
+  )
+SET(CMAKE_CXX_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_Fortran_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
+SET(CMAKE_ASM_TARGET_INCLUDE_PATH ${CMAKE_C_TARGET_INCLUDE_PATH})
---
+++ PA0/build/CMakeFiles/Tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/Tutorial.dir/src/engine.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/graphics.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/camera.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/window.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/main.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/object.cpp.o"
+  "CMakeFiles/Tutorial.dir/src/shader.cpp.o"
+  "Tutorial.pdb"
+  "Tutorial"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/Tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
