ef6dac43e90ab6b14816d12aa07f785e38495c54
Stefan Stukelja <sstukelja@nevada.unr.edu>
2016-09-25 20:13:37 -0700

    Object Made

---
+++ PA4/include/object.h
@@ -0,0 +1,30 @@
+#ifndef OBJECT_H
+#define OBJECT_H
+
+#include <vector>
+#include "graphics_headers.h"
+
+class Object
+{
+  public:
+    Object();
+    ~Object();
+    void Update(unsigned int dt, bool direction, int pause);
+    void Render();
+
+    glm::mat4 GetModel();
+    glm::mat4 GetModel2();
+
+  private:
+    glm::mat4 model;
+    glm::mat4 model2;
+    std::vector<Vertex> Vertices;
+    std::vector<unsigned int> Indices;
+    GLuint VB;
+    GLuint IB;
+
+    float angle;
+    float stopAngle;
+};
+
+#endif /* OBJECT_H */
